a1e966358efb55807a42f566280c3740561ea57a
Shubham Prajapat <shubham.prajapat@hiteshi.com>
2024-08-07 17:48:23 +0530

    Made Auth section responsive for all screen types.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,99 +4,126 @@ import PasswordInput from "@/assets/images";
 import { validateEmail } from "@/helper/FormHelper";
 import { loginApi, loginWithPasswordApi } from "@/utils/api";
 import Link from "next/link";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import leftImg from "../../assets/images/left.png"
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import leftImg from "../../assets/images/left.png";
 import Image from "next/image";
-import logoImg from "../../assets/images/logo.png"
+import logoImg from "../../assets/images/logo.png";
+import CommonButton from "@/components/Common/CommonButton";
 
-const Login=()=>{
-    const router = useRouter();
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const [error,setError] = useState({email:'',password:''});
-    useEffect(()=>{
-        if(localStorage.getItem("token")) {
-        router.push('/dashboard', { scroll: false })
-        }
-    })
-    const submit= async ()=>{
-        if(!validateEmail(email)) {
-            setError({...error,email:'Please enter valid email'});
-        } else if(password === "") {
-            setError({...error,password:'Please enter valid password'});
-        } else {
-            const result = await loginWithPasswordApi({email:email,password:password});
-            if(result.status === 200) {
-                localStorage.setItem("token",result.body.token);
-                localStorage.setItem("userInfo",JSON.stringify(result.body.user));
-                router.push('/dashboard', { scroll: false })
-            } else {
-                toast.error(result.error);
-            }
-        }
+const Login = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState({ email: "", password: "" });
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      router.push("/dashboard", { scroll: false });
     }
-    return (<div className="h-screen bg-gradient">
-    <div className="flex h-full items-center justify-center">
+  });
+  const submit = async () => {
+    if (!validateEmail(email)) {
+      setError({ ...error, email: "Please enter valid email" });
+    } else if (password === "") {
+      setError({ ...error, password: "Please enter valid password" });
+    } else {
+      const result = await loginWithPasswordApi({
+        email: email,
+        password: password,
+      });
+      if (result.status === 200) {
+        localStorage.setItem("token", result.body.token);
+        localStorage.setItem("userInfo", JSON.stringify(result.body.user));
+        router.push("/dashboard", { scroll: false });
+      } else {
+        toast.error(result.error);
+      }
+    }
+  };
+  return (
+    <div className="h-screen bg-gradient">
+      <div className="flex h-full items-center justify-center">
         <div className=" w-[40%] hidden md:block">
-        <Image src={leftImg} className="object-cover" alt="Globe side" />
+          <Image src={leftImg} className="object-cover" alt="Globe side" />
         </div>
         <div className="flex-1 flex items-center justify-center md:w-82% w-[100%]">
-        <div className="bg-white p-8 rounded-lg shadow-lg md:w-[30%] w-[80%] right-[10%] top-[20%] md:absolute"> {/* Changed width to w-96 */}
-      <div className="flex justify-center mb-4">
-        <Image src={logoImg} alt="The Box Cycle Logo" />
-      </div>
-      <h1 className="text-[35px] font-[600] text-center">Login</h1>
-      <p className="text-center mb-[50px]">Please login to continue to your account</p>
-            <div className="mb-5">
-                <Input
-                    name="email"
-                    value={email}
-                    onChange={(e)=>{
-                        if(validateEmail((e.target as HTMLInputElement).value)) {
-                            setError({...error,email:''});
-                        }
-                        setEmail((e.target as HTMLInputElement).value)
-                    }}
-                    error={error.email}
-                    label="Email"
-                    className={''}
-                />
+          <div className="bg-white px-8 pb-8 md:pb-10 md:px-16 lg:pb-8 2xl:pt-4 xl:pb-12 xl:px-20 rounded-lg shadow-lg 2xl:w-[600px]">
+            <div className="flex justify-center xl:mb-2">
+              <Image
+                src={logoImg}
+                alt="The Box Cycle Logo"
+                className="!w-[50%] md:max-w-[100%]"
+              />
             </div>
-            <div className="mb-5">
-                <PasswordInput 
-                    name="password"
-                    value={password}
-                    onChange={(e)=>{
-                        setError({...error,password:''});
-                        setPassword((e.target as HTMLInputElement).value)
-                    }}
-                    error={error.password}
-                    label="Password"
-                    className={''}
-                />
-            </div>
-            <button type="submit" onClick={submit} className="w-full p-2 bg-green-800 text-white rounded-[10px] hover:bg-green-700 mb-4">
-                Login
-            </button>
-            <div className="flex items-center justify-center mb-4">
-                <span className="border-t border-gray-300 flex-grow mr-2"></span>
-                <span className="text-gray-500">or</span>
-                <span className="border-t border-gray-300 flex-grow ml-2"></span>
-            </div>
-            <button onClick={()=>{router.push('/')}} className="w-full p-2 border-2 border-green-800 text-green-800 rounded-[10px] hover:bg-gray-100 mb-4">
-                Login with OTP
-            </button>
-            <p className="text-center sm:text[10] text-[18px]">
-                Don't have an account yet? <Link href={'/signup'} className="text-[#000000] font-semibold">Sign Up</Link>
+            <h1 className="text-[24px] md:text-[28px] xl:text-[32px] font-medium text-center">
+              Login
+            </h1>
+            <p className="text-center mb-6 lg:mb-2 xl:mb-10 font-light text-[14px] xl:text-[16px]">
+              Please login to continue to your account
             </p>
+            <div className="mb-5">
+              <Input
+                name="email"
+                value={email}
+                onChange={(e) => {
+                  if (validateEmail((e.target as HTMLInputElement).value)) {
+                    setError({ ...error, email: "" });
+                  }
+                  setEmail((e.target as HTMLInputElement).value);
+                }}
+                error={error.email}
+                label="Email"
+                className={""}
+              />
+            </div>
+            <div className="mb-5">
+              <PasswordInput
+                name="password"
+                value={password}
+                onChange={(e) => {
+                  setError({ ...error, password: "" });
+                  setPassword((e.target as HTMLInputElement).value);
+                }}
+                error={error.password}
+                label="Password"
+                className={""}
+              />
+            </div>
+            <CommonButton
+              type="submit"
+              className="w-full p-2 bg-green-800 text-white rounded-[10px] hover:bg-green-700 mb-3 lg:mb-2 xl:mb-4"
+              onClick={submit}
+            >
+              {"Login"}
+            </CommonButton>
+            <div className="flex items-center justify-center mb-4">
+              <span className="border-t border-gray-300 flex-grow mr-2"></span>
+              <span className="text-gray-500">or</span>
+              <span className="border-t border-gray-300 flex-grow ml-2"></span>
+            </div>
+
+            <CommonButton
+              className="w-full p-2 border-2 border-green-800 text-green-800 rounded-[10px] hover:bg-gray-100 mb-4"
+              onClick={() => {
+                router.push("/");
+              }}
+            >
+              Login with OTP
+            </CommonButton>
+            <p className="text-center text-[12px] sm:text-[15px] xl:text-[17px]">
+              Don't have an account yet?{" "}
+              <Link href={"/signup"} className="text-[#000000] font-semibold">
+                Sign Up
+              </Link>
+            </p>
+          </div>
         </div>
-        </div>
+      </div>
+      <ToastContainer />
     </div>
-    <ToastContainer />
-    </div>);
-}
+  );
+};
 
 export default Login;
--- src/components/Auth/SignUp/UserForm/index.tsx
+++ src/components/Auth/SignUp/UserForm/index.tsx
@@ -4,137 +4,194 @@ import PasswordInput from "@/assets/images";
 import { validateEmail, validatePassword } from "@/helper/FormHelper";
 import { signupApi } from "@/utils/api";
 import Link from "next/link";
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import logoImg from "../../../../assets/images/logo.png"
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import logoImg from "../../../../assets/images/logo.png";
 import Image from "next/image";
-const UserForm=({changeMode}:{changeMode:(email:string)=>void})=>{
-    const [disabled,setDisabled] = useState(false);
-    const [formData,setFormData] = useState({
-        firstName:'',
-        lastName:'',
-        email:'',
-        password:'',
-        confirmPassword:'',
-      })
-      const [formError,setFormError] = useState({
-        firstName:'',
-        lastName:'',
-        email:'',
-        password:'',
-        confirmPassword:'',
-      })
-      
-      const submit= async ()=>{
-        if(formData.firstName==='') {
-            setFormError({...formError,firstName:'Please enter first name'});
-        } else  if(formData.lastName==='') {
-            setFormError({...formError,lastName:'Please enter last name'});
-        } else if(!validateEmail(formData.email)) {
-            setFormError({...formError,email:'Please enter valid email'});
-        } else if(!validatePassword(formData.password)) {
-            setFormError({...formError,password:'Password must contain at least one capital letter, one special character, and be at least 8 characters long.'});
-        } else if(formData.confirmPassword==='') {
-            setFormError({...formError,confirmPassword:'Please enter confirm password'});
-        } else if(formData.password !== formData.confirmPassword) {
-            setFormError({...formError,confirmPassword:'Password and confirm password not match'});
-        } else {
-            try {
-                setDisabled(true);
-                const result = await signupApi(formData);
-                if(result.status === 200) {
-                    setDisabled(false);
-                    changeMode(formData.email);
-                } else {
-                    setDisabled(false);
-                    toast.error(result.error)
-                }
-              } catch (err) {
-                console.log('Err',err);
-              }
-    
-        }
-      }
+import CommonButton from "@/components/Common/CommonButton";
+const UserForm = ({ changeMode }: { changeMode: (email: string) => void }) => {
+  const [disabled, setDisabled] = useState(false);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [formError, setFormError] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-    return(
-        <div className="bg-white p-8 rounded-lg shadow-lg md:w-[30%] w-[80%] right-[10%] top-[20%] md:absolute"> 
-            <div className="flex justify-center mb-4">
-                <Image src={logoImg} alt="The Box Cycle Logo" className="sm:h-auto h-[100px]" />
-            </div>
-            <div className="mb-5">
-                <Input 
-                    name="firstName"
-                    value={formData.firstName}
-                    onChange={(e)=>{
-                        setFormError({...formError,firstName:''});
-                        setFormData({...formData,firstName:(e.target as HTMLInputElement).value})
-                    }}
-                    error={formError.firstName}
-                    label="First Name"
-                    className={formError.firstName ? 'border-[red] border-[1px] sm:h-auto h-[40px]' : 'sm:h-auto h-[40px]'}
-                />
-            </div>
-            <div className="mb-5">
-                <Input 
-                    name="lastName"
-                    value={formData.lastName}
-                    onChange={(e)=>{
-                        setFormError({...formError,lastName:''});
-                        setFormData({...formData,lastName:(e.target as HTMLInputElement).value})
-                    }}
-                    error={formError.lastName}
-                    label="Last Name"
-                    className={formError.lastName ? 'border-[red] border-[1px] sm:h-auto h-[40px]' : 'sm:h-auto h-[40px]'}
-                />
-            </div>
-            <div className="mb-5">
-              <Input 
-                    name="email"
-                    value={formData.email}
-                    onChange={(e)=>{
-                        setFormError({...formError,email:''});
-                        setFormData({...formData,email:(e.target as HTMLInputElement).value})
-                    }}
-                    error={formError.email}
-                    label="Email"
-                    className={formError.email ? 'border-[red] border-[1px] sm:h-auto h-[40px]' : 'sm:h-auto h-[40px]'}
-                />
-            </div>
-            <div className="mb-5">
-              <PasswordInput 
-                name="password"
-                value={formData.password}
-                onChange={(e)=>{
-                    setFormError({...formError,password:'',confirmPassword:''});
-                    setFormData({...formData,password:(e.target as HTMLInputElement).value})
-                }}
-                error={formError.password}
-                label="Password"
-                className={formError.password ? 'border-[red] border-[1px] sm:h-auto h-[40px]' : 'sm:h-auto h-[40px]'}
-              />
-            </div>
-            <div className="mb-5">
-              <PasswordInput 
-                name="confirmPassword"
-                value={formData.confirmPassword}
-                onChange={(e)=>{
-                    setFormError({...formError,password:'',confirmPassword:''});
-                    setFormData({...formData,confirmPassword:(e.target as HTMLInputElement).value})
-                }}
-                error={formError.confirmPassword}
-                label="Confirm Password"
-                className={formError.confirmPassword ? 'border-[red] border-[1px] sm:h-auto h-[40px]' : 'sm:h-auto h-[40px]'}
-              />
-            </div>
-            <button type="submit"  onClick={submit} disabled={disabled} className="w-full p-2 bg-green-800 text-white rounded-[10px] hover:bg-green-700 mb-4">
-                Sign Up
-            </button>
-            <p className="text-center sm:text[10] text-[18px]">
-            Already have an account? <Link href={'/'} className="text-[#000000] font-semibold">Login</Link>
-            </p>
-            <ToastContainer />
-        </div>
-    );
-}
+  const submit = async () => {
+    if (formData.firstName === "") {
+      setFormError({ ...formError, firstName: "Please enter first name" });
+    } else if (formData.lastName === "") {
+      setFormError({ ...formError, lastName: "Please enter last name" });
+    } else if (!validateEmail(formData.email)) {
+      setFormError({ ...formError, email: "Please enter valid email" });
+    } else if (!validatePassword(formData.password)) {
+      setFormError({
+        ...formError,
+        password:
+          "Password must contain at least one capital letter, one special character, and be at least 8 characters long.",
+      });
+    } else if (formData.confirmPassword === "") {
+      setFormError({
+        ...formError,
+        confirmPassword: "Please enter confirm password",
+      });
+    } else if (formData.password !== formData.confirmPassword) {
+      setFormError({
+        ...formError,
+        confirmPassword: "Password and confirm password not match",
+      });
+    } else {
+      try {
+        setDisabled(true);
+        const result = await signupApi(formData);
+        if (result.status === 200) {
+          setDisabled(false);
+          changeMode(formData.email);
+        } else {
+          setDisabled(false);
+          toast.error(result.error);
+        }
+      } catch (err) {
+        console.log("Err", err);
+      }
+    }
+  };
+
+  return (
+    <div className="bg-white px-8 pb-8 md:pb-10 md:px-12 lg:pb-8 2xl:pt-4 xl:pb-12 2xl:px-20 rounded-lg shadow-lg max-w-[400px] xs:w-[500px] md:w-[90%] lg:w-[500px] 2xl:w-[600px]">
+      <div className="flex justify-center xl:mb-2">
+        <Image
+          src={logoImg}
+          alt="The Box Cycle Logo"
+          className="!w-[50%] md:max-w-[100%]"
+        />
+      </div>
+      <div className="mb-4">
+        <Input
+          name="firstName"
+          value={formData.firstName}
+          onChange={(e) => {
+            setFormError({ ...formError, firstName: "" });
+            setFormData({
+              ...formData,
+              firstName: (e.target as HTMLInputElement).value,
+            });
+          }}
+          error={formError.firstName}
+          label="First Name"
+          className={
+            formError.firstName
+              ? "border-[red] border-[1px] sm:h-auto h-[40px]"
+              : "sm:h-auto h-[40px]"
+          }
+        />
+      </div>
+      <div className="mb-4">
+        <Input
+          name="lastName"
+          value={formData.lastName}
+          onChange={(e) => {
+            setFormError({ ...formError, lastName: "" });
+            setFormData({
+              ...formData,
+              lastName: (e.target as HTMLInputElement).value,
+            });
+          }}
+          error={formError.lastName}
+          label="Last Name"
+          className={
+            formError.lastName
+              ? "border-[red] border-[1px] sm:h-auto h-[40px]"
+              : "sm:h-auto h-[40px]"
+          }
+        />
+      </div>
+      <div className="mb-4">
+        <Input
+          name="email"
+          value={formData.email}
+          onChange={(e) => {
+            setFormError({ ...formError, email: "" });
+            setFormData({
+              ...formData,
+              email: (e.target as HTMLInputElement).value,
+            });
+          }}
+          error={formError.email}
+          label="Email"
+          className={
+            formError.email
+              ? "border-[red] border-[1px] sm:h-auto h-[40px]"
+              : "sm:h-auto h-[40px]"
+          }
+        />
+      </div>
+      <div className="mb-4">
+        <PasswordInput
+          name="password"
+          value={formData.password}
+          onChange={(e) => {
+            setFormError({ ...formError, password: "", confirmPassword: "" });
+            setFormData({
+              ...formData,
+              password: (e.target as HTMLInputElement).value,
+            });
+          }}
+          error={formError.password}
+          label="Password"
+          className={
+            formError.password
+              ? "border-[red] border-[1px] sm:h-auto h-[40px]"
+              : "sm:h-auto h-[40px]"
+          }
+        />
+      </div>
+      <div className="mb-4">
+        <PasswordInput
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={(e) => {
+            setFormError({ ...formError, password: "", confirmPassword: "" });
+            setFormData({
+              ...formData,
+              confirmPassword: (e.target as HTMLInputElement).value,
+            });
+          }}
+          error={formError.confirmPassword}
+          label="Confirm Password"
+          className={
+            formError.confirmPassword
+              ? "border-[red] border-[1px] sm:h-auto h-[40px]"
+              : "sm:h-auto h-[40px]"
+          }
+        />
+      </div>
+      <CommonButton
+        type="submit"
+        className="w-full p-2 bg-green-800 text-white rounded-[10px] hover:bg-green-700 mb-3 lg:mb-2 xl:mb-4"
+        onClick={submit}
+        disabled={disabled}
+      >
+        {"Sign Up"}
+      </CommonButton>
+      <p className="text-center text-[12px] sm:text-[15px] xl:text-[17px]">
+        Already have an account?{" "}
+        <Link href={"/"} className="text-[#000000] font-semibold">
+          Login
+        </Link>
+      </p>
+      <ToastContainer />
+    </div>
+  );
+};
 
 export default UserForm;
